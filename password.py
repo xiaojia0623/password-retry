e6912cc66cb956b772de0753572a91c1778e6651
Michelle CAO <jiahu0724417@gmail.com>
2020-08-10 18:21:47 +0800

    basic version done

---
+++ password.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+i = 3
+while True:
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功!!')
+		break  # break 逃出迴圈
+	else:
+		i -= 1
+		print('密碼錯誤!還有',i,'次機會!')
+		if i == 0:
+			break # 如果i變0 ，自動跳出
